4be31cec2e48085a7b83c77c479a0525a94d6742
Johnmarare <mararejohn@gmail.com>
2024-01-02 16:50:48 +0300

    fix: formatting

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,26 +1,25 @@
 #!/usr/bin/python3
-"""
-Gather data from an api
-"""
+""" For a given employee, returns information about the TODO list progress"""
 import requests
 from sys import argv
 
+
 if __name__ == '__main__':
-    emp_id = argv[1]
-    burl = "https://jsonplaceholder.typicode.com/users/{}/todos".format(emp_id)
+    id_ = argv[1]
+    url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(id_)
 
     s = requests.Session()
 
-    url = "https://jsonplaceholder.typicode.com/users/{}".format(emp_id)
-    response = s.get(url)
+    url2 = 'https://jsonplaceholder.typicode.com/users/{}'.format(id_)
+    response = s.get(url2)
     name = response.json()['name']
 
-    response = s.get(burl)
+    response = s.get(url)
     body = response.json()
     tasks_done = []
     for b in body:
-        if b.get('completed', False):
-            tasks_done.append('\t' + b['title'])
+        if b['completed']:
+            tasks_done.append('\t ' + b['title'])
 
     print('Employee {} is done with tasks({}/{}):'
           .format(name, len(tasks_done), len(body)))
