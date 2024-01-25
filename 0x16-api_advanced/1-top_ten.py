96bc5b83941e8393877e55ca75ed5999a14664c4
Johnmarare <mararejohn@gmail.com>
2024-01-25 12:11:53 +0300

    find prob

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,7 +9,7 @@ def top_ten(subreddit):
     """
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/John_Marare)"
+        "User-Agent": "API Project by John_Marare"
     }
     params = {"limit": 10}
     response = requests.get(url, headers=headers, params=params,
