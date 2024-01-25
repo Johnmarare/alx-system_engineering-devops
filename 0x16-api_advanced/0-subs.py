fe055861f7e93f7cecf19e33d5b1c5cb05f8706e
Johnmarare <mararejohn@gmail.com>
2024-01-25 11:26:14 +0300

    api request

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,14 +10,14 @@ def number_of_subscribers(subreddit):
     sureddit: to be queried
     Return: Number of subs
     """
+    if subreddit is None:
+        return (0)
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/John_Marare)"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
+    results = response.json()
+    subs = results.get("data", {}).get("subscribers", 0)
 
-    if response.status_code == 404:
-        return 0
-    results = response.json().get("data")
-
-    return results.get("subscribers")
+    return subs
