4d9053fe3d7f92cefdfbe89e84d5c06e604c5d00
Johnmarare <mararejohn@gmail.com>
2024-08-25 17:42:18 +0000

    recurse debug

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,30 +1,35 @@
 #!/usr/bin/python3
-"""Function to query a list of all hot posts on a given Reddit subreddit."""
+"""
+Module Docs
+"""
 import requests
 
 
-def recurse(subreddit, hot_list=[], after="", count=0):
-    """Returns a list of titles of all hot posts on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/John_marare)"
+def recurse(subreddit, hot_list=[], count=0, after=None):
+    """
+    Function Docs
+    """
+    url = 'https://www.reddit.com'
+    header = {
+        'Accept': 'application/json',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
     }
-    params = {
-        "after": after,
-        "count": count,
-        "limit": 100
-    }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-    if response.status_code == 404:
-        return None
-
-    results = response.json().get("data")
-    after = results.get("after")
-    count += results.get("dist")
-    for c in results.get("children"):
-        hot_list.append(c.get("data").get("title"))
-
-    if after is not None:
-        return recurse(subreddit, hot_list, after, count)
-    return hot_list
+    if after is None:
+        after = ''
+    response = requests.get(
+            '{}/r/{}/.json?sort={}&limit={}&count={}&after={}'.format(
+                url, subreddit, 'hot', 30, count, after),
+            headers=header,
+            allow_redirects=False)
+    if response.status_code == 200:
+        posts = response.json()['data']['children']
+        hot_list.extend(list(map(lambda x: x['data']['title'], posts)))
+        if len(posts) >= 30 and response.json()['data']['after']:
+            return recurse(subreddit, hot_list,
+                           count + len(posts),
+                           response.json()['data']['after']
+                           )
+        else:
+            return hot_list if hot_list else None
+    else:
+        return hot_list if hot_list else None
